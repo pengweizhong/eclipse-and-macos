ab4ef73d9304525ac3597fc8b3aef1f24d0edd32
GitHub Action <action@github.com>
2024-02-01 02:25:39 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "eclipse-and-macos"
